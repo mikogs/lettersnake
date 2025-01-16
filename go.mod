c63940d03ef36e4e2cebdab975f07d3fc4fc643d
Mikolaj Gasior <m@gasior.dev>
2025-01-16 22:56:59 +0100

    Tidy up (#2)
    * WIP
    
    * Tidy up libraries
    
    * Fix animals dictionary

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/cli-games/snakey-letters
 go 1.23.4
 
 require (
+	github.com/go-phings/broccli v2.0.0+incompatible
 	github.com/go-phings/terminal-ui v0.2.1
-	github.com/mikolajgs/broccli v2.0.0+incompatible
 )
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/mikolajgs/broccli"
+	"github.com/go-phings/broccli"
 )
 
 func versionHandler(c *broccli.CLI) int {
@@ -16,6 +17,7 @@ func main() {
 	cli := broccli.NewCLI("snakey-letters", "Classic snake but with letters and words!", "")
 	cmd := cli.AddCmd("start", "Starts the game", startHandler)
 	cmd.AddFlag("words", "f", "", "Text file with wordlist", broccli.TypePathFile, broccli.IsExistent|broccli.IsRequired)
+	cmd.AddFlag("speed", "s", "", "Snake speed", broccli.TypeInt, 0)
 	_ = cli.AddCmd("version", "Shows version", versionHandler)
 	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
 		os.Args = []string{"App", "version"}
@@ -28,8 +30,15 @@ func startHandler(c *broccli.CLI) int {
 	g.readWordsFromFile(c.Flag("words"))
 	g.randomizeWords()
 
+	speed := c.Flag("speed")
+	if speed == "" {
+		speed = "200"
+	}
+
+	speedInt, _ := strconv.Atoi(speed)
+
 	gi := newGameInterface(g)
-	gi.setSpeed(200)
+	gi.setSpeed(speedInt)
 
 	gi.run()
 	return 0
